d340d429816d499bdfc5a16ef567f2a1c4d0b14a
Mike Ton <mike.ton@gmail.com>
2014-02-20 16:01:47 -0800

    Render Target WIP : Ethereal Spirit Works!
    Former-commit-id: 3732ba82a190003ecbd1e63fc20ef95b9933f6f2

--- Unity/Assets/Shared/Scripts/mton/Camera/mt_Camera_RT.js
+++ Unity/Assets/Shared/Scripts/mton/Camera/mt_Camera_RT.js
@@ -6,11 +6,11 @@
 @script AddComponentMenu("Image Effects/mton_PIXL")
 @script RequireComponent(Camera)
 
-private var shaderCamera    : GameObject;
+private var shaderCamera    : Camera;
+private var shaderCamera_m  : Camera;
 public  var replaceShader   : Shader;
 public  var layerMaskString : String;
 
-protected var mainRnTexture : RenderTexture; //main camera RenderTexture
 private var scaleResolution : float = 0.125;
 
 function Update(){
@@ -24,25 +24,42 @@ function Update(){
 }
 
 /*** HACK: Function can not be generalized...only operates on explicit RenderTexture :(  ***/
-
 protected var renderTexture : RenderTexture;
 
 function initRendTexture(){
-  renderTexture            = RenderTexture.GetTemporary (camera.pixelWidth * scaleResolution, camera.pixelHeight * scaleResolution, 8) ; //Allocate a temporary render texture.
-  renderTexture.filterMode = FilterMode.Point                                                                      ; 
+  renderTexture            = RenderTexture.GetTemporary (camera.pixelWidth, camera.pixelHeight, 8) ; //Allocate a temporary render texture.
+  renderTexture.filterMode = FilterMode.Point                                                      ;
 }
 
 function nullRendTexture():void{ //HACK: Function can not be generalized...only operates on explicit RenderTexture :(
   if (renderTexture != null) {
-    RenderTexture.ReleaseTemporary (renderTexture);
-    renderTexture = null;
+    RenderTexture.ReleaseTemporary (renderTexture) ;
+    renderTexture = null                           ;
+  }
+}
+/*** HACK: Function can not be generalized...only operates on explicit RenderTexture :(  ***/
+
+
+/*** HACK: Function can not be generalized...only operates on explicit RenderTexture :(  ***/
+protected var renderTexture_m : RenderTexture;
+
+function initRendTexture_m(){
+  renderTexture_m            = RenderTexture.GetTemporary (camera.pixelWidth * scaleResolution, camera.pixelHeight * scaleResolution, 8) ; //Allocate a temporary render texture.
+  renderTexture_m.filterMode = FilterMode.Point                                                                                          ;
+}
+
+function nullRendTexture_m():void{ //HACK: Function can not be generalized...only operates on explicit RenderTexture :(
+  if (renderTexture_m != null) {
+    RenderTexture.ReleaseTemporary (renderTexture_m) ;
+    renderTexture_m = null                           ;
   }
 }
 /*** HACK: Function can not be generalized...only operates on explicit RenderTexture :(  ***/
 
 function OnDisable() {	
-  DestroyImmediate (shaderCamera);
-  nullRendTexture();
+  DestroyImmediate (shaderCamera) ;
+  nullRendTexture()               ;
+  nullRendTexture_m()             ;
 }
 
 // --------------------------------------------------------
@@ -50,58 +67,77 @@ function OnDisable() {
 function Start() {
   // Disable if we don't support image effects
   if (!SystemInfo.supportsImageEffects) {
-    enabled = false;
-    return;
+    enabled = false ;
+    return          ;
   }
 
 }
 
 // --------------------------------------------------------
 
+function initShaderCam(shaderCamera : Camera, renderTexture : RenderTexture):Camera{
+
+  if (!shaderCamera) {
+    var tempObj:GameObject = new GameObject("ShaderCamera", Camera) ;
+    tempObj.hideFlags      = HideFlags.HideAndDontSave              ;
+    shaderCamera           = tempObj.camera                         ;
+    //shaderCamera.depth   = camera.depth + 1                       ;
+    shaderCamera.enabled   = false                                  ;
+  }
+  shaderCamera.CopyFrom (camera)                                    ;
+  shaderCamera.targetTexture = renderTexture                        ;
+  //shaderCamera.Render()                                           ; // ??? Do I need this on??? : No, if RenderWithShader(); else yes.
+
+/*
+//Generating DepthNormal Info
+shaderCamera.depthTextureMode = DepthTextureMode.DepthNormals      ; //activate camera depth gen
+var depthNormalShader : Shader = Shader.Find("Hidden/Camera-DepthNormalTexture");
+shaderCamera.RenderWithShader (depthNormalShader,null); //if replacement tag == "", all objects in scene rendered with replacment shaders	
+*/
+
+  return shaderCamera;
+
+}
+
 function OnPreRender()
 {
   if (!enabled || !gameObject.active)
     return;
-    
+
   nullRendTexture();
   initRendTexture();
-  
-  if (!shaderCamera) {
-    shaderCamera                = new GameObject("ShaderCamera", Camera) ;
-    shaderCamera.camera.depth   = camera.depth + 1;
-    shaderCamera.camera.enabled = false                                  ;
-    shaderCamera.hideFlags      = HideFlags.HideAndDontSave              ;
-  }
-  var cam = shaderCamera.camera                             ;
-  cam.CopyFrom (camera)                                     ;
-  cam.backgroundColor    = Color(0,0,0,0)                   ;
-  cam.clearFlags         = CameraClearFlags.SolidColor      ;
-  //cam.clearFlags       = CameraClearFlags.Depth           ;
-  //cam.clearFlags       = CameraClearFlags.Nothing         ; //Don't clear anything
-  cam.targetTexture      = renderTexture                    ;
-  //cam.Render()                                            ; // ??? Do I need this on?
-  
+
+  nullRendTexture_m();
+  initRendTexture_m();
+
+  shaderCamera             = initShaderCam(shaderCamera, renderTexture)     ;
+  shaderCamera.cullingMask = ~(1<<(LayerMask.NameToLayer(layerMaskString))) ; //short cut : find layer by String : ~invert everything but that layer : pass bit to culling Mask
+  shaderCamera.depth       = -2                                             ;
+  shaderCamera.Render()                                                     ;
+
+  shaderCamera_m                 = initShaderCam(shaderCamera_m, renderTexture_m) ;
+  shaderCamera.depth             = -1                                             ;
+  shaderCamera_m.backgroundColor = Color(0,0,0,0)                                 ;
+  shaderCamera_m.clearFlags      = CameraClearFlags.SolidColor                    ;
+  //shaderCamera_m.clearFlags    = CameraClearFlags.Depth                         ;
+  //shaderCamera_m.clearFlags    = CameraClearFlags.Nothing                       ; //Don't clear anything
+
   //---Transform Level---//
-  //cam.cullingMask = 1<<(LayerMask.NameToLayer(layerMaskString)); //short cut : find layer by Strin : ~invert everything but that layer : pass bit to culling Mask
-  
+  shaderCamera_m.cullingMask = 1<<(LayerMask.NameToLayer(layerMaskString)); //short cut : find layer by String : ~invert everything but that layer : pass bit to culling Mask
+
   //---Material Level---//
-  cam.RenderWithShader (replaceShader,"RenderType_TON"); //if replacement tag == "", all objects in scene rendered with replacment shaders	replaceShader
-  
-  /*
-  //Generating DepthNormal Info
-  cam.depthTextureMode = DepthTextureMode.DepthNormals      ; //activate camera depth gen
-  var depthNormalShader : Shader = Shader.Find("Hidden/Camera-DepthNormalTexture");
-  cam.RenderWithShader (depthNormalShader,null); //if replacement tag == "", all objects in scene rendered with replacment shaders	
-  */
-  
+  //shaderCamera_m.RenderWithShader (replaceShader,"RenderType_TON"); //if replacement tag == "", all objects in scene rendered with replacment shaders	replaceShader
+  shaderCamera_m.RenderWithShader (replaceShader,""); //if replacement tag == "", all objects in scene rendered with replacment shaders	replaceShader
+
 }
 
 // Called by the camera to apply the image effect
 function OnRenderImage (source : RenderTexture, destination : RenderTexture) : void{
 
   doOnRenderImage(renderTexture, destination);
-  
-  nullRendTexture();
+
+  nullRendTexture()   ;
+  nullRendTexture_m() ;
 
 }
 
@@ -109,3 +145,4 @@ function doOnRenderImage(source : RenderTexture, destination : RenderTexture) : 
   Graphics.Blit(renderTexture, destination); // mat); 
   //renderTexture.DiscardContents(true, true); //??? does nothing
 } 
+
