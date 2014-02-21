3732ba82a190003ecbd1e63fc20ef95b9933f6f2
Mike Ton <mike.ton@gmail.com>
2014-02-20 16:01:47 -0800

    Render Target WIP : Ethereal Spirit Works!

--- Unity/Assets/Shared/Scripts/mton/Camera/mt_Camera_RT_Comp.js
+++ Unity/Assets/Shared/Scripts/mton/Camera/mt_Camera_RT_Comp.js
@@ -13,7 +13,7 @@ public class mt_Camera_RT_Comp extends mt_Camera_RT {
     super.Start();
   }
 
-  private var m_CompositeMaterial : Material;
+  private var m_CompositeMaterial         : Material;
   private function GetCompositeMaterial() : Material {
     if (m_CompositeMaterial == null) {
       m_CompositeMaterial           = new Material( compositeShader ) ; //Create a temporary Material from a Shader.
@@ -23,48 +23,48 @@ public class mt_Camera_RT_Comp extends mt_Camera_RT {
   } 
 
   function doOnRenderImage(source : RenderTexture, destination : RenderTexture) : void {
-    var compositeMat:Material = GetCompositeMaterial()               ;
-    //compositeMat.SetTexture ("_EthrTex", ethTexture)                 ;
-    compositeMat.SetTexture ("_EthrTex", renderTexture)                 ;
-    //compositeMat.SetTexture ("_EthrTex", renderTexture.depthBuffer)                 ;
-    compositeMat.SetTexture ("_MaskTex", mskTexture)                 ;
-    compositeMat.SetTexture ("_NormTex", nrmTexture)                 ;
-    ImageEffects.BlitWithMaterial(compositeMat, source, destination) ;
-    //Graphics.Blit(renderTexture, destination)                      ; // mat) ;
-    //Graphics.Blit(delTexture, destination)                         ; // mat) ;
+    var compositeMat:Material = GetCompositeMaterial()                ;
+    //compositeMat.SetTexture ("_EthrTex", ethTexture)                ;
+    compositeMat.SetTexture ("_EthrTex", renderTexture_m)             ;
+    //compositeMat.SetTexture ("_EthrTex", renderTexture.depthBuffer) ;
+    compositeMat.SetTexture ("_MaskTex", mskTexture)                  ;
+    compositeMat.SetTexture ("_NormTex", nrmTexture)                  ;
+    ImageEffects.BlitWithMaterial(compositeMat, source, destination)  ;
+    //Graphics.Blit(renderTexture, destination)                       ; // mat) ;
+    //Graphics.Blit(delTexture, destination)                          ; // mat) ;
   }
 
+/*
+private static function Render4TapQuad( dest : RenderTexture, offsetX : float, offsetY : float ) : void {
+GL.Begin( GL.QUADS );
 
-  private static function Render4TapQuad( dest : RenderTexture, offsetX : float, offsetY : float ) : void {
-    GL.Begin( GL.QUADS );
+// Direct3D needs interesting texel offsets!		
+var off = Vector2.zero;
+if( dest != null )
+off = dest.GetTexelOffset() * 0.75;
 
-    // Direct3D needs interesting texel offsets!		
-    var off = Vector2.zero;
-    if( dest != null )
-      off = dest.GetTexelOffset() * 0.75;
+Set4TexCoords( off.x, off.y, offsetX, offsetY ) ;
+GL.Vertex3( 0,0, 0.1 )                          ;
 
-    Set4TexCoords( off.x, off.y, offsetX, offsetY ) ;
-    GL.Vertex3( 0,0, 0.1 )                          ;
+Set4TexCoords( 1.0 + off.x, off.y, offsetX, offsetY ) ;
+GL.Vertex3( 1,0, 0.1 )                                ;
 
-    Set4TexCoords( 1.0 + off.x, off.y, offsetX, offsetY ) ;
-    GL.Vertex3( 1,0, 0.1 )                                ;
+Set4TexCoords( 1.0 + off.x, 1.0 + off.y, offsetX, offsetY ) ;
+GL.Vertex3( 1,1, 0.1 )                                      ;
 
-    Set4TexCoords( 1.0 + off.x, 1.0 + off.y, offsetX, offsetY ) ;
-    GL.Vertex3( 1,1, 0.1 )                                      ;
+Set4TexCoords( off.x, 1.0 + off.y, offsetX, offsetY ) ;
+GL.Vertex3( 0,1, 0.1 )                                ;
 
-    Set4TexCoords( off.x, 1.0 + off.y, offsetX, offsetY ) ;
-    GL.Vertex3( 0,1, 0.1 )                                ;
+GL.End();
+}
 
-    GL.End();
-  }
-
-  private static function Set4TexCoords( x : float, y : float, offsetX : float, offsetY : float ) : void
-  {
-    GL.MultiTexCoord2( 0, x - offsetX, y - offsetY );
-    GL.MultiTexCoord2( 1, x + offsetX, y - offsetY );
-    GL.MultiTexCoord2( 2, x + offsetX, y + offsetY ); 
-    GL.MultiTexCoord2( 3, x - offsetX, y + offsetY );
-  }
-
+private static function Set4TexCoords( x : float, y : float, offsetX : float, offsetY : float ) : void
+{
+GL.MultiTexCoord2( 0, x - offsetX, y - offsetY );
+GL.MultiTexCoord2( 1, x + offsetX, y - offsetY );
+GL.MultiTexCoord2( 2, x + offsetX, y + offsetY ); 
+GL.MultiTexCoord2( 3, x - offsetX, y + offsetY );
+}
+*/
 
 }
