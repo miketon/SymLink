7635434c8784600972ae89f1da29979eaec86bcd
Mike Ton <mike.ton@gmail.com>
2014-02-27 07:55:45 -0800

    Moving Editor Tools Over.  And modding Physics character package.
    Former-commit-id: ec3b931b02fefa2b8bb2bc2ab507bf15dc88d07c

---
+++ Unity/Assets/Shared/Scripts/mton/Physics_Platformer_Kit_Icarus/__go.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class __go : MonoBehaviour {
+
+  protected Transform xform ;
+
+  protected virtual void Awake(){
+		doAwake();
+  }
+
+	protected virtual void doAwake(){
+    xform = transform; //caching component lookup: transform == GetComponent(transform)
+	}
+
+}
