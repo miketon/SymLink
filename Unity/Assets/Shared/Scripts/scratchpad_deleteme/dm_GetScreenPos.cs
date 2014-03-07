682536e6f1b930eaee9c03e567b336f21751c622
Mike Ton <mike.ton@gmail.com>
2014-03-07 14:58:55 -0800

    Tweaking Lancer run.
    Former-commit-id: 60c45f9531b224dc690bc44bb17e358297d1fd4f

---
+++ Unity/Assets/Shared/Scripts/scratchpad_deleteme/dm_GetScreenPos.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dm_GetScreenPos : MonoBehaviour {
+    
+    private Transform xform;
+    public Transform xformFollow;
+    public Vector3 offSet;
+    public TextMesh txt;
+    private Camera cam;
+
+	// Use this for initialization
+	void Start () {
+	  txt.text = "Hello World";
+	  xform = transform;
+	  cam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	  if(xformFollow){
+	    xform.position = xformFollow.position + offSet;
+	    Vector3 viewPos = cam.WorldToViewportPoint(xform.position);
+	    txt.text = ("y: " + viewPos.y + "/nx:" + viewPos.x);
+	  }
+	}
+}
