7684565408d974ad6d84d8c4ba7fc92a59153d28
Mike Ton <mike.ton@gmail.com>
2014-04-12 14:44:34 -0700

    Adding Voxel Knight
    Former-commit-id: 04d25fcb82802bbfaf43e0ad652275d0dae713f2

--- Unity/Assets/Shared/Scripts/mton/GameObject/Unit/onEmit/mt_EmitRotate.cs
+++ Unity/Assets/Shared/Scripts/mton/GameObject/Unit/onEmit/mt_EmitRotate.cs
@@ -4,25 +4,25 @@ using System.Collections;
 [RequireComponent(typeof(AudioSource))]
 public class mt_EmitRotate : MonoBehaviour { //put me on same transform as animator object
 
-  private Transform xform    ;
-  private Vector3   spawnPos ;
-  private Quaternion   spawnRot ;
-  public  float     spinScale = 90.0f;
+  private Transform  xform    ;
+  private Vector3    spawnPos ;
+  private Quaternion spawnRot ;
 
-	public bool bRotate = true;
+  public float spinScale = 90.0f ;
+  public bool  bRotate   = true  ;
 
   void Awake(){
-    xform = transform;
-		spawnPos = xform.position;
-		spawnRot = xform.rotation;
+    xform    = transform      ;
+    spawnPos = xform.position ;
+    spawnRot = xform.rotation ;
   }
-  
-	void Update(){
-		float magDist = (spawnPos - xform.position).magnitude + 3.5f;
-		if(bRotate){
-			xform.Rotate((Vector3.right * spinScale * Time.deltaTime)*(magDist)); // * magDist);
-		}
-		xform.Translate(Vector3.forward * Time.deltaTime);
-	}
+
+  void Update(){
+    float magDist = (spawnPos - xform.position).magnitude + 3.5f;
+    if(bRotate){
+      xform.Rotate((Vector3.right * spinScale * Time.deltaTime)*(magDist)); // * magDist);
+    }
+    xform.Translate(Vector3.forward * Time.deltaTime);
+  }
 
 }
