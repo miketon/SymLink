04d25fcb82802bbfaf43e0ad652275d0dae713f2
Mike Ton <mike.ton@gmail.com>
2014-04-12 14:44:34 -0700

    Adding Voxel Knight

---
+++ Unity/Assets/Shared/Scripts/mton/GameObject/Utilities/mt_DistanceFromSpawned.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine        ;
+using System.Collections ;
+
+public class mt_DistanceFromSpawned : MonoBehaviour { //put me on same transform as animator object
+
+  private float magDist = 0.0f;
+
+  private Transform xform                   ;
+  private Vector3   spawnPos = Vector3.zero ;
+  private bool      bSpawned = false        ;
+
+  void Awake(){
+    xform    = transform      ;
+  }
+
+	void OnEnable(){
+    spawnPos = xform.position ;
+	}
+
+  void Update(){
+    if(bSpawned){
+      magDist = (spawnPos - xform.position).magnitude + 3.5f;
+      Debug.Log(" magDist : " + magDist);
+    }
+  }
+
+  void OnBecameVisible () { //object must have mesh render
+    bSpawned = true;
+  }
+
+  void OnBecameInvisible () {
+    bSpawned = false;
+  }
+
+}
