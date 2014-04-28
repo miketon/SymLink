af6243d9ddec9610fdbd2077551dab3236b6e2fb
Mike Ton <mike.ton@gmail.com>
2014-04-27 20:57:44 -0700

    Adding custom ControlFreak functionality. Recognizes stick JustPress, Pressed, and JustRelease.  Also converting Avatar to SetupMachine rig.
    Former-commit-id: 851b7817a6c60157411b0a308440d577e58a8620

--- Unity/Assets/Shared/Scripts/projects/Physics_Platformer_Kit_Icarus/PlayerMove_mton_jump.cs
+++ Unity/Assets/Shared/Scripts/projects/Physics_Platformer_Kit_Icarus/PlayerMove_mton_jump.cs
@@ -5,7 +5,7 @@ public class PlayerMove_mton_jump : PlayerMove_mton{
 
 	//push player at jump force
 	public override void Jump(Vector3 jumpVelocity){
-	    print("I am jumping.");
+	    Debug.Log("I am jumping.");
 		base.Jump(jumpVelocity);
 		//gridForce();
 	}
---
+++ Unity/Assets/Shared/Scripts/projects/Physics_Platformer_Kit_Icarus/PlayerMove_mton_jump_audio.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove_mton_jump_audio : PlayerMove_mton_jump{
+
+	private AudioSource audioSource_H;
+	private AudioSource audioSource_V;
+	private AudioSource audioSource_JUMP;
+	public GameObject oAx_H;
+	public GameObject oAx_V;
+	public GameObject oJump;
+
+	public float hPitch = 2.0f;
+	public float hPan = 2.0f;
+	public float hVolume = 1.0f;
+
+	// map audio to touch movement
+	public override void io_Touch(){
+		base.io_Touch();
+		doAudioTouch();
+	    //print("I am filtering audio." + this.__h);
+	}
+
+	void doAudioTouch(){
+		float vAxis = Mathf.Clamp(this.__v, -1.0f, 0.5f);
+		audioSource_H.volume =   this.__h *  __h * hVolume;
+		audioSource_V.volume = vAxis * vAxis;
+		audioSource_H.pan =   this.__h * hPan;
+		audioSource_H.pitch = Mathf.Abs(  this.__h) * hPitch; // (  this.__h * 0.5f) + 1.0f;
+		audioSource_V.pan = vAxis * 2.0f;
+		audioSource_V.pitch = 2.0f + -vAxis;
+	}
+
+	public override void doStart(){
+		base.doStart();
+		audioSource_H = oAx_H.GetComponent<AudioSource>();
+		audioSource_V = oAx_V.GetComponent<AudioSource>();
+		audioSource_JUMP = oJump.GetComponent<AudioSource>();
+		audioSource_H.loop = true;
+		audioSource_V.loop = true;
+		audioSource_H.Play();
+		audioSource_V.Play();
+	}
+/*	
+	public override void doUpdate(){
+	  base.doUpdate();
+	}
+*/
+}
+
