df9606ab63a94b2eef24c6b14eb038bc4f3ebd1f
Mike Ton <mike.ton@gmail.com>
2014-05-01 14:11:57 -0700

    WIP : Modal Control adding bAttack, bJump and bClear
    Former-commit-id: e711c1e86414ef675b14136a5ca64788ca600dcf

--- Unity/Assets/Shared/Scripts/projects/InputControlFreak/TouchStick_mton.cs
+++ Unity/Assets/Shared/Scripts/projects/InputControlFreak/TouchStick_mton.cs
@@ -20,11 +20,12 @@ public class TouchStick_mton : MonoBehaviour{
   // Controller Variable 
   // ----------------------
 
-  public  TouchController	ctrl                           ;
-  public  GUIElement buttona_A_GUI                       ;
-  public  GUIElement buttona_B_GUI                       ;
-  public  GUIElement buttona_C_GUI                       ;
-  public  GUIElement buttona_D_GUI                       ;
+  public  TouchController	ctrl   ;
+  public  PlayerMove_mton   mton   ;
+  public  GUIElement buttona_A_GUI ;
+  public  GUIElement buttona_B_GUI ;
+  public  GUIElement buttona_C_GUI ;
+  public  GUIElement buttona_D_GUI ;
 
   public  float buttonScale  = 0.1f  ;
   public  float buttonOffSet = 1.75f ;
@@ -59,10 +60,10 @@ public class TouchStick_mton : MonoBehaviour{
       pauseZone         = this.ctrl.GetZone(ZONE_PAUSE)          ;
 
       // mton Stuff
-      screenRes.x               = Screen.width                                                        ;
-      screenRes.y               = Screen.height                                                       ;
-      screenAspRatio            = screenRes.x/screenRes.y                                             ;
-      cRadius                   = stick_attackStick.GetScreenRad ()                                   ; ///screenRes.y                 ;
+      screenRes.x               = Screen.width                      ;
+      screenRes.y               = Screen.height                     ;
+      screenAspRatio            = screenRes.x/screenRes.y           ;
+      cRadius                   = stick_attackStick.GetScreenRad () ; ///screenRes.y ;
 
       buttona_A_GUI.transform.localScale = new Vector3(buttonScale, buttonScale * screenAspRatio, buttonScale) ; //scale adjust to screen ratio
       buttona_A_GUI.enabled              = false                                                               ;
@@ -75,13 +76,23 @@ public class TouchStick_mton : MonoBehaviour{
     }
   }
 
-  void doModalButton(GUIElement goGUI, float goPos){
+  void doModalButton(GUIElement goGUI, int indexE){
+
     Vector2 pStick           = stick_attackStick.GetScreenPos()                                                              ;
     //Vector3 p              = Camera.main.ScreenToWorldPoint(new Vector3(pStick.x, screenRes.y-pStick.y, 5.0f))             ; //matching go to cam projection ; 5.0f is a guess.
     Vector2 screenPosNorm    = new Vector2(pStick.x/screenRes.x, 1.0f-pStick.y/screenRes.y)                                  ;
-    Vector3 p                = new Vector3(screenPosNorm.x+cRadius/screenRes.x* buttonOffSet * goPos, screenPosNorm.y, 0.0f) ;
+    Vector3 radialPos        = indexToRadialPosition(indexE, 4)                                                              ;
+    //Vector3 p              = new Vector3(screenPosNorm.x+cRadius/screenRes.x* buttonOffSet * goPos, screenPosNorm.y, 0.0f) ;
+    Vector3 p                = new Vector3(radialPos.x + screenPosNorm.x, radialPos.y + screenPosNorm.y, 0.0f)               ;
     goGUI.transform.position = p                                                                                             ;
     goGUI.enabled            = true                                                                                          ;
+  }
+
+  Vector3 indexToRadialPosition(int indexE, int indexMax){
+    float angle = 2 * Mathf.PI / indexMax * indexE                     ; //indexEtry == 0 == right ; counterclockwise rotation
+    float x_i   = cRadius/screenRes.x* buttonOffSet * Mathf.Cos(angle) ;
+    float y_i   = cRadius/screenRes.y* buttonOffSet * Mathf.Sin(angle) ;
+    return new Vector3(x_i, y_i, 0.0f)                                 ;
   }
 
   // ----------------------
@@ -118,29 +129,31 @@ public class TouchStick_mton : MonoBehaviour{
              this.ctrl.GetZone(ZONE_PAUSE).SetRect(new Rect(rectFals.x, rectFals.y, -32,64)) ;
              */
         }
-        else if(stick_attackDir == TouchStick.StickDir.U){
-          doModalButton(buttona_C_GUI, 1.0f) ;
-          buttona_A_GUI.enabled = false      ;
-          buttona_B_GUI.enabled = false      ;
-          buttona_D_GUI.enabled = false      ;
+
+        else if(stick_attackDir == TouchStick.StickDir.R){  
+          doModalButton(buttona_A_GUI, 0) ; //0 index == right ; counterclockwise rotation
+          buttona_B_GUI.enabled = false   ;
+          buttona_C_GUI.enabled = false   ;
+          buttona_D_GUI.enabled = false   ;
+		  mton.doAttack();
         }
-        else if(stick_attackDir == TouchStick.StickDir.R){
-          doModalButton(buttona_A_GUI, 1.0f) ;
-          buttona_B_GUI.enabled = false      ;
-          buttona_C_GUI.enabled = false      ;
-          buttona_D_GUI.enabled = false      ;
+        else if(stick_attackDir == TouchStick.StickDir.U){
+          doModalButton(buttona_C_GUI, 1) ;
+          buttona_A_GUI.enabled = false   ;
+          buttona_B_GUI.enabled = false   ;
+          buttona_D_GUI.enabled = false   ;
         }
         else if(stick_attackDir == TouchStick.StickDir.L){
-          doModalButton(buttona_B_GUI, -1.0f) ;
-          buttona_A_GUI.enabled = false       ;
-          buttona_C_GUI.enabled = false       ;
-          buttona_D_GUI.enabled = false       ;
+          doModalButton(buttona_B_GUI, 2) ;
+          buttona_A_GUI.enabled = false   ;
+          buttona_C_GUI.enabled = false   ;
+          buttona_D_GUI.enabled = false   ;
         }
         else if(stick_attackDir == TouchStick.StickDir.D){
-          doModalButton(buttona_D_GUI, -1.0f) ;
-          buttona_A_GUI.enabled = false       ;
-          buttona_B_GUI.enabled = false       ;
-          buttona_C_GUI.enabled = false       ;
+          doModalButton(buttona_D_GUI, 3) ;
+          buttona_A_GUI.enabled = false   ;
+          buttona_B_GUI.enabled = false   ;
+          buttona_C_GUI.enabled = false   ;
         }
       }
       else if(stick_attackStick.JustReleased()){
@@ -149,6 +162,7 @@ public class TouchStick_mton : MonoBehaviour{
         buttona_B_GUI.enabled = false           ;
         buttona_C_GUI.enabled = false           ;
         buttona_D_GUI.enabled = false           ;
+		mton.clearBoolState();
       }
 
       // ----------------
