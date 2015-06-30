b3a8e8b5d229cab24fb8a66a429b891595f92dfc
Mike Ton <mike.ton@gmail.com>
2015-06-30 12:38:58 +1000

    Updating FrameWork

--- Unity/Assets/Shared/__MtonFrameWork/__Code/__Interface/__iGlobal.cs
+++ Unity/Assets/Shared/__MtonFrameWork/__Code/__Interface/__iGlobal.cs
@@ -5,14 +5,23 @@ namespace MTON{
 
   public class mtonGlobal{ //Project Global object to hold systemwide paths, layers, tags ...etc
 
+    //handles Layers - Entities can move
+	  public const string _PLAYER = "Player";
+	  public const string _ENEMY  = "Enemy";
+	  public const string _BULLET = "Bullet";
+	  public const string _ITEMS  = "Item";
+
+    //handles Layers - Environments 
+  	public const string _FLOOR  = "Floor";
+  	public const string _WALLS  = "Walls";
+  	public const string _DOORS  = "Doors"; //could be spawn, save, restore, entry, exit points
+
+    //handles Tags
     //handles Paths
     public static string _char = "_Characters/" ;
     public static string _sdFX = "_SoundFX/"    ;
     public static string _txtr = "_Textures/"   ;
 	  public static string _matl = "_Materials/"  ;
-
-    //handles Layers
-    //handles Tags
 
   }
 
