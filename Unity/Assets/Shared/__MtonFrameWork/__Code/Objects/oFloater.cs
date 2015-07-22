490afe649da34e62db8aa0bdb38bb95fa3e43e6d
Mike Ton <mike.ton@gmail.com>
2015-07-21 19:23:17 -0700

    oFloater auto finds player.
    But if player isn’t on Player on start…it will fail.  Race Condition.

--- Unity/Assets/Shared/__MtonFrameWork/__Code/Objects/oFloater.cs
+++ Unity/Assets/Shared/__MtonFrameWork/__Code/Objects/oFloater.cs
@@ -14,6 +14,12 @@ namespace MTON.codeObjects{
 	public Color cActv = Color.red   ;
 	public Color cRest = Color.green ;
 
+	public float fSpeed = 1.0f; //speed
+	public float fAmplt = 1.0f; //amplitude
+	public float fFreqn = 1.0f; //frequency
+
+	private static LayerMask __layerPlayer  ;
+	private bool bPlayerLeft = true ; //is the player to the left; move right if oFloater is in wave pattern mode
 	private Transform xform;
 	private Renderer rendr ;
     private cTween    tw   ;
@@ -21,23 +27,43 @@ namespace MTON.codeObjects{
 	public float distThreshold = 3.0f;
 	public float durTime = 1.0f;
 
-	public void Awake(){
+	public virtual void Awake(){
+	    __layerPlayer = LayerMask.GetMask (__gCONSTANT._PLAYER);
 		xform = this.GetComponent<Transform>();
 		rendr = this.dispObj.GetComponent<Renderer> ();
         tw = __gUtility.AddComponent_mton<cTween>(this.dispObj.gameObject)   ; //Tweening display obj vs. character controller
 	}
 
-	public virtual void FixedUpdate(){
-	  float dist = Vector3.Distance(this.xform.position, this.player.position);
-//	  Debug.Log("Dist : " + dist);
-	  if(dist > distThreshold){ //Activate
-		AI_Actv(true);
-		if(bFollow){
-			DoFollow();
-		}
+	public virtual void Start(){
+      __gUtility.CheckAndInitLayer(this.gameObject, __gCONSTANT._ENEMY) ; // HACK :level triggers/hint should ignore ground raycast/collision check!
+
+	}
+
+	public virtual void OnEnable(){
+	  if(this.player == null){
+	    Collider[] hitColliders = Physics.OverlapSphere(this.xform.position, 5.0f, __layerPlayer) ;
+	    for(int i=0 ; i<hitColliders.Length; i++) {
+		  this.player = hitColliders[i].transform;
+		  Debug.Log(this + " FOUND PLAYER : " + this.player);
+	    }
 	  }
-	  else{ //Rest
-	    AI_Actv(false);
+	}
+
+	public virtual void OnDisable(){}
+
+	public virtual void FixedUpdate(){
+	  if(this.player != null){
+	    float dist = Vector3.Distance(this.xform.position, this.player.position);
+//	    Debug.Log("Dist : " + dist);
+	    if(dist > distThreshold){ //Activate
+		  AI_Actv(true);
+		  if(bFollow){
+			DoFollow();
+		  }
+	    }
+	    else{ //Rest
+	      AI_Actv(false);
+	    }
 	  }
 	}
 
