ea2f1f7f78e9957ee3e7edce5a3f97c68939fde9
Mike Ton <mike.ton@gmail.com>
2015-07-31 12:02:35 -0700

    WIP Audio emission.  Background Soundtrack going.

---
+++ Unity/Assets/Shared/__MtonFrameWork/__Code/Classes/cEmit_Audio.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine        ;
+using System.Collections ;
+using MTON.Interface     ;
+using MTON.Class         ;
+using MTON.Global        ;
+
+public class cEmit_Audio : MonoBehaviour, IEmit<Rigidbody>{ 
+
+	public  Sound snd           ;
+	public  bool  bAnim = false ; // Should I subscribe to cAnimn delegates
+	private cAnimn an           ;
+
+	private void Start(){
+	  an = this.GetComponent<cAnimn>();
+	  if(an == true){
+	    bAnim = true ;
+	  }
+	  else{
+	    bAnim = false ;
+	  }
+	}
+
+	private void OnEnable(){}
+	private void OnDisable(){}
+
+#region iEmit implementation
+
+  public void Init(){  
+
+  }
+  public void Play(){
+
+  }
+  public void Stop(){
+
+  }
+
+  public void OnComplete(){
+
+  }
+
+#endregion
+}
