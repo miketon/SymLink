34cb4655c66a8764350f4c17ae5c03257b0468d2
Mike Ton <mike.ton@gmail.com>
2015-09-02 21:56:39 -0700

    HeatSeek setting up two different moveProp structures for front and behind.

--- Unity/Assets/Shared/__MtonFrameWork/__Code/Classes/cEmit_HeatSeek.cs
+++ Unity/Assets/Shared/__MtonFrameWork/__Code/Classes/cEmit_HeatSeek.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine        ;
+using System             ; //Must use for [Serializable] attr
 using System.Collections ;
 using MTON.Class         ;
 using MTON.Global        ;
@@ -28,18 +29,36 @@ public class cEmit_HeatSeek : cEmit_Bullet {
 	private void doBHind(bool bHind){
 	  if(bHind){
 	    this.transform.DOScale(this.inScl * 2.0f, 0.15f).SetEase(this.bHindCurve);
+		this.mvC = this.mvB;
 	  }
 	  else{
 	    this.transform.DOScale(this.inScl, 0.25f).SetEase(this.bHindCurve);
+		this.mvC = this.mvF;
 	  }
 	}
+//
+//	public float rotationSpeed = 1.0f;
+//    public float moveSpeed = 1.0f;
+//	public float distLockd = 1.0f; //decrease rotation
+//	public float dist = 1.0f;
+//	public float rotMult = 1.0f;
+//	public float posMult = 1.0f;
 
-	public float rotationSpeed = 1.0f;
-    public float moveSpeed = 1.0f;
-	public float distLockd = 1.0f; //decrease rotation
-	public float dist = 1.0f;
-	public float rotMult = 1.0f;
-	public float posMult = 1.0f;
+	public  s_MoveProperties mvF = new s_MoveProperties(); //move Front  Properties
+	public  s_MoveProperties mvB = new s_MoveProperties(); //move Behind Properties
+	private s_MoveProperties mvC = new s_MoveProperties(); //move Current Properties
+    [Serializable] //MUST : add so that this custom data type can be displayed in the inspector
+	public struct s_MoveProperties{
+		
+	  public float rotationSpeed ;
+      public float moveSpeed     ;
+	  public float distLockd     ; //decrease rotation
+	  public float dist          ;
+	  public float rotMult       ;
+	  public float posMult       ;
+
+    }
+
 	public Vector3 v3Dir;
 
 	public override void Awake (){
@@ -48,15 +67,15 @@ public class cEmit_HeatSeek : cEmit_Bullet {
 	}
 
 	void Start(){
-	  moveSpeed = UnityEngine.Random.Range(moveSpeed, moveSpeed * 5.5f);
-	  this.rotationSpeed = UnityEngine.Random.Range(this.rotationSpeed, this.rotationSpeed * 1.5f);
+	  mvC.moveSpeed = UnityEngine.Random.Range(mvC.moveSpeed, mvC.moveSpeed * 5.5f);
+	  this.mvC.rotationSpeed = UnityEngine.Random.Range(this.mvC.rotationSpeed, this.mvC.rotationSpeed * 1.5f);
 	  target = MTON.Global.__gCONSTANT._LEVEL.mPlayer;
 	  this._player = this.target.GetComponent<oPlayer>();
 	}
 
 	// Update is called once per frame
 	void Update () {
-//		this.transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.LookRotation(this.target.position - this.transform.position), this.rotationSpeed * Time.deltaTime);
+//		this.transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.LookRotation(this.target.position - this.transform.position), this.mvC.rotationSpeed * Time.deltaTime);
 		v3Dir = transform.position - target.position;
 		if(this._player){
 		  if(this._player.bFaceRt){
@@ -76,44 +95,53 @@ public class cEmit_HeatSeek : cEmit_Bullet {
 			}
 		  }
 		}
-		dist = v3Dir.magnitude;
-		if(dist < distLockd){
-		  rotMult = 1.0f-(dist/distLockd);
-		  posMult += 1.0f-(dist/distLockd);
+		mvC.dist = v3Dir.magnitude;
+		if(mvC.dist < mvC.distLockd){
+		  mvC.rotMult = 1.0f-(mvC.dist/mvC.distLockd);
+		  mvC.posMult += 1.0f-(mvC.dist/mvC.distLockd);
 		}
 		else{
-		  rotMult = 1.0f;
-		  posMult = 1.0f;
+		  mvC.rotMult = 1.0f;
+		  mvC.posMult = 1.0f;
 		}
 
         float angle = Mathf.Atan2(v3Dir.y, v3Dir.x) * Mathf.Rad2Deg;
 //		Quaternion newRotation = Quaternion.AngleAxis(angle, Vector3.right);
 		Quaternion newRotation = Quaternion.AngleAxis(angle, Vector3.forward) * Quaternion.Euler(new Vector3(0.0f, -90.0f, 0.0f));
 
-		transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * this.rotationSpeed * rotMult);
-		transform.position += transform.forward * this.moveSpeed * Time.deltaTime * posMult;
+		transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * this.mvC.rotationSpeed * mvC.rotMult);
+		transform.position += transform.forward * this.mvC.moveSpeed * Time.deltaTime * mvC.posMult;
 		transform.SetPosZ(0.0f);
 	}
 
-	public cLevel.e_Enmy enemySpawnOnComplete;
-	public cLevel.fx_Hit enemySpawnFX;
+
+	public s_OnCompleteProperties sOC = new s_OnCompleteProperties();
+    [Serializable] //MUST : add so that this custom data type can be displayed in the inspector
+    public struct s_OnCompleteProperties{
+
+	  public bool bCompleteSpawn;
+	  public cLevel.e_Enmy enemySpawnOnComplete;
+	  public cLevel.fx_Hit enemySpawnFX;
+	  public cLevel.e_Anim onCompleteFX;
+
+    }
 
 	public override void OnComplete (){
 		base.OnComplete ();
 		Debug.Log ("HEAT SEEKER COMPLETE ! " + this);
+		if(this.sOC.bCompleteSpawn){
 		if(this.bHind){
-	      if(this.enemySpawnOnComplete != cLevel.e_Enmy.None){
-		    __gCONSTANT._LEVEL.SpawnObj(this.enemySpawnOnComplete, this.transform.position, Quaternion.identity, (Transform SpawnedObj)=>{
+	      if(this.sOC.enemySpawnOnComplete != cLevel.e_Enmy.None){
+		    __gCONSTANT._LEVEL.SpawnObj(this.sOC.enemySpawnOnComplete, this.transform.position, Quaternion.identity, (Transform SpawnedObj)=>{
 		      float randomF = UnityEngine.Random.Range(1.0f, 3.0f) ;
               Vector3 PositionSpawn = SpawnedObj.position + Vector3.up * 0.5f * randomF   ; // lift slightly off ground to allow for spin and pop
 			  SpawnedObj.position = PositionSpawn; // reuse for Spawn Effect
-			  __gCONSTANT._LEVEL.fx_Dust(this.enemySpawnFX, PositionSpawn);
+			  __gCONSTANT._LEVEL.fx_Dust(this.sOC.enemySpawnFX, PositionSpawn);
               return true                                                 ;
 		    });
 		  }
 		}
-		else{
-		  __gCONSTANT._LEVEL.fx_Dust(cLevel.fx_Hit.GunFlar_00, this.transform.position);
 		}
+		__gCONSTANT._LEVEL.fx_Dust(this.sOC.onCompleteFX, this.transform.position);
 	}
 }
