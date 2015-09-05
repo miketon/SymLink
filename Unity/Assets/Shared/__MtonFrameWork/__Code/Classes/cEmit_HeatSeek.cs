59f1f090c24a5a7a51a2d1453d2442ec488a3ab0
Mike Ton <mike.ton@gmail.com>
2015-09-04 22:51:20 -0700

    Reoganizing HeatSeek.

--- Unity/Assets/Shared/__MtonFrameWork/__Code/Classes/cEmit_HeatSeek.cs
+++ Unity/Assets/Shared/__MtonFrameWork/__Code/Classes/cEmit_HeatSeek.cs
@@ -74,43 +74,59 @@ public class cEmit_HeatSeek : cEmit_Bullet {
 	  this.si = __gUtility.AddComponent_mton<cSight>(this.gameObject)  ; 
 	}
 
-	// Update is called once per frame
-	void Update () {
-//		this.transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.LookRotation(this.target.position - this.transform.position), this.mvC.rotationSpeed * Time.deltaTime);
-		Vector3 v3Dir = transform.position - target.position;
-		if(this._player){
-		  if(this._player.bFaceRt){
-		    if(v3Dir.x < 0.0f){
-			  this.bHind = true;
-			}
-		    else{
-			  this.bHind = false;
-			}
+	bool bPlayerBehind(oPlayer IN_PLAYER, Vector3 IN_kDIR){
+	  bool retBHind = false;
+	  if(this.si.doViewConeCheck(IN_PLAYER.transform)){ //within vision cone??
+	    if(IN_PLAYER.bFaceRt){
+		  if(IN_kDIR.x < 0.0f){
+		    retBHind = true;
 		  }
 		  else{
-		    if(v3Dir.x > 0.0f){
-			  this.bHind = true;
-			}
-			else{
-			  this.bHind = false;
-			}
+		    retBHind = false;
+	      }
+	    }
+	    else{
+	      if(IN_kDIR.x > 0.0f){
+		    retBHind = true;
 		  }
-		}
-		mvC.dist = v3Dir.magnitude;
-		if(mvC.dist < mvC.distLockd){
-		  mvC.rotMult = 1.0f-(mvC.dist/mvC.distLockd);
-		  mvC.posMult += 1.0f-(mvC.dist/mvC.distLockd);
-		}
-		else{
-		  mvC.rotMult = 1.0f;
-		  mvC.posMult = 1.0f;
-		}
+		  else{
+		    retBHind = false;
+		  }
+	    }
+	  }
+	  return retBHind;
+	}
 
-        float angle = Mathf.Atan2(v3Dir.y, v3Dir.x) * Mathf.Rad2Deg;
+	void rotateTowards(Vector3 IN_DIR){
+
+		float angle = Mathf.Atan2(IN_DIR.y, IN_DIR.x) * Mathf.Rad2Deg;
 //		Quaternion newRotation = Quaternion.AngleAxis(angle, Vector3.right);
 		Quaternion newRotation = Quaternion.AngleAxis(angle, Vector3.forward) * Quaternion.Euler(new Vector3(0.0f, -90.0f, 0.0f));
 
 		transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * this.mvC.rotationSpeed * mvC.rotMult);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+//		this.transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.LookRotation(this.target.position - this.transform.position), this.mvC.rotationSpeed * Time.deltaTime);
+		if(this._player){
+		  Vector3 v3Dir = transform.position - target.position;
+//		  this.rotateTowards(v3Dir);
+		  this.bHind = bPlayerBehind(this._player, v3Dir);
+
+ 		  mvC.dist = v3Dir.magnitude;
+		  if(mvC.dist < mvC.distLockd){
+		    mvC.rotMult = 1.0f-(mvC.dist/mvC.distLockd);
+		    mvC.posMult += 1.0f-(mvC.dist/mvC.distLockd);
+		  }
+		  else{
+		    mvC.rotMult = 1.0f;
+		    mvC.posMult = 1.0f;
+		  }
+		}
+
+		//move Forward
 		transform.position += transform.forward * this.mvC.moveSpeed * Time.deltaTime * mvC.posMult;
 		transform.SetPosZ(0.0f);
 	}
