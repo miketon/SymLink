54792e854b5b310162203b4c0083047f6a84eaf1
Mike Ton <mike.ton@gmail.com>
2015-09-07 15:40:00 -0700

    Cleaning up cSpawn Delegate logic.

--- Unity/Assets/Shared/__MtonFrameWork/__Code/Classes/cSpawn.cs
+++ Unity/Assets/Shared/__MtonFrameWork/__Code/Classes/cSpawn.cs
@@ -7,21 +7,14 @@ using MTON.Global        ;
 namespace MTON.Class{
   public class cSpawn : MonoBehaviour {
 
-    public delegate void OnEMIT(Transform IN_XFORM, int IN_INDEX) ; //set up delegate
+    public delegate void OnEMIT(Transform IN_XFORM, int IN_OBJ, int IN_FP) ; //set up delegate
     public delegate void OnRAPD(bool bRapd) ; //set up delegate
 	public OnEMIT OnEmitDelegate            ; //delegate instance
-	public OnEMIT OnFlarDelegate            ; //delegate instance
 	public OnRAPD OnRapdDelegate            ; //delegate instance
 
-	public virtual void doEmit(Transform IN_XFORM, int IN_INDEX){ //on Bullet Emission (projectile)
+	public virtual void doEmit(Transform IN_XFORM, int IN_OBJ, int IN_FP){ //on Bullet Emission (projectile)
 	  if(OnEmitDelegate != null){ // NOTE: Just in case class exist, but no delegate is assigned
-	    OnEmitDelegate(IN_XFORM, IN_INDEX);
-      }
-	}
-
-	public virtual void doFlar(Transform IN_XFORM, int IN_INDEX){ //on Firing Point Emission (flare)
-	  if(OnFlarDelegate != null){ // NOTE: Just in case class exist, but no delegate is assigned
-	    OnFlarDelegate(IN_XFORM, IN_INDEX);
+	    OnEmitDelegate(IN_XFORM, IN_OBJ, IN_FP);
       }
 	}
 
@@ -30,6 +23,71 @@ namespace MTON.Class{
 	    OnRapdDelegate(bRapd);
       }
 	}
+
+#region SINGLESHOT ---
+
+    public virtual void doSinglFire(bool bAttk, bool IN_FACEFORWARD=true){
+      if(bAttk){
+        if(this.sEM.firePnts.Length > 0){ // Firing Points exist
+          if(this.sEM.eBlt.Length > 0){   // Bullets exist
+		    int        indexBL = this.sBL_mod.iIndex                      ; //which Bullet Object to launch
+		    int        indexFP = this.sFP_mod.iIndex                      ; //which Firing Point to emit from
+		    Transform  firePnt = this.sEM.firePnts[indexFP]               ; 
+            Quaternion fireRot = firePnt.rotation                         ;
+            if(IN_FACEFORWARD == false){                                    //Brute force guessing; Understanding of matrix not high enough
+              Vector3 vRot = firePnt.rotation.eulerAngles                 ;
+              vRot         = new Vector3(vRot.x, vRot.y + 180.0f, vRot.z) ; //MAGIC NUMBER : Why y = 180.0f ??? Likely related to parent -x scale
+              fireRot      = Quaternion.Euler(vRot)                       ;
+            }
+            firePnt.gameObject.SetActive(true)                            ;
+		    this.doEmit(firePnt, indexBL, indexFP)                        ;
+
+            this.sFP_mod.doMod()                                          ; //modulate to next firing Point
+            this.sBL_mod.doMod()                                          ; //modulate to next bullet
+          } 
+	    }
+	  }
+	}
+
+#endregion
+
+#region RAPIDFIRE ---
+
+	private bool brapd         = false ;
+    public virtual void doRapidFire(bool bRapd, bool IN_BGROUND=true){
+	  this.doRapd(bRapd); //trigger delegate
+	  this.brapd = bRapd;
+      if(bRapd == true){
+        StartCoroutine(WhileRapidFire());
+      }
+    }
+
+    public IEnumerator WhileRapidFire(){
+      while(this.brapd == true){
+        //          an.hState = cAnimn.eStateH.Plnt;
+        if(this.mt_TimeStep(this.sEM.fireRate)){
+          //	      Debug.Log ("Rapid Fire : " + Time.time); //HACK : time print doesn't match fireRate why???
+          this.doSinglFire(true);
+        }
+        else{
+          this.doSinglFire(false);
+        }
+        yield return null;
+      }
+    }
+
+    private float stepDrtn = 0.0f;
+    private bool mt_TimeStep(float stepIncm){
+      if(Time.time > stepDrtn){
+        stepDrtn  = Time.time + stepIncm ;
+        return true                      ;    
+      }
+      else{
+        return false                     ;
+      }
+    }
+	
+#endregion
 
 #region EmitterProperties and Modulation Structures
 
@@ -68,108 +126,8 @@ namespace MTON.Class{
         }
       }
 
-#endregion
-
-    public virtual void doAttk(bool bAttk, bool IN_FACEFORWARD=true){
-      if(bAttk){
-        if(this.sEM.firePnts.Length > 0){
-          //            an.attkST          = cAnimn.eStateB.DN                    ;
-		  int indexFP = this.sFP_mod.iIndex;
-		  Transform firePnt  = this.sEM.firePnts[indexFP]               ; 
-          Quaternion fireRot = firePnt.rotation                         ;
-          if(IN_FACEFORWARD == false){                                    //Brute force guessing; Understanding of matrix not high enough
-            Vector3 vRot = firePnt.rotation.eulerAngles                 ;
-            vRot         = new Vector3(vRot.x, vRot.y + 180.0f, vRot.z) ; //MAGIC NUMBER : Why y = 180.0f ??? Likely related to parent -x scale
-            fireRot      = Quaternion.Euler(vRot)                       ;
-          }
-          firePnt.gameObject.SetActive(true)                            ;
-		  this.doEmit(firePnt, indexFP)                                 ;
-          if(this.sEM.eBlt.Length > 0){
-            if(this.sEM.eBlt[this.sBL_mod.iIndex] != cLevel.e_Bllt.None){ //Firing actual bullets
-              __gCONSTANT._LEVEL.Emit_Bullet(this.sEM.eBlt[this.sBL_mod.iIndex], firePnt.position, fireRot, (Transform xForm)=>{
-                  cEmit_Bullet cBullet = xForm.gameObject.GetComponent<cEmit_Bullet>() ;
-                  if(cBullet){
-                    cBullet.OnComplete();
-                  }
-                  return xForm ;
-              })               ;
-            }
-          }
-          if(this.sEM.eGun != cLevel.fx_Hit.None){ // Flare : set to -1 to prevent emission
-            __gCONSTANT._LEVEL.Emit_pFX(this.sEM.eGun, firePnt.position, Quaternion.identity, (Transform xForm)=>{
-                firePnt.gameObject.SetActive(false)                                  ;
-                return xForm ;
-            })               ;
-          }
-        }
-        this.sFP_mod.doMod();
-        this.sBL_mod.doMod();
-      }
-      else{
-        if(this.brapd){ 
-          //            an.attkST = cAnimn.eStateB.PW   ; //Power up attack
-        }
-        else{
-          //            an.attkST = cAnimn.eStateB.Idle ;
-        }
-      }
-    }
-
-#region RAPIDFIRE ---
-
-	private bool brapd         = false ;
-    private bool bDpdX         = true  ;
-    private bool bDpdY         = false ;
-
-    public virtual void doRapidFire(bool bRapd, bool IN_BGROUND=true){
-	  this.brapd = bRapd;
-	  this.doRapd(bRapd); //trigger delegate
-      if(bRapd == true){
-        StartCoroutine(WhileRapidFire());
-        this.bDpdX = false; //dPad x ignore
-        this.bDpdY = true ; //dPad y listen
-//        if(IN_BGROUND == true){
-//          __gCONSTANT._LEVEL.fx_Dust(this.sEM.eDld, this.transform.position, true);
-//        }
-      }
-      else{
-        this.bDpdX = true  ; //dPad x listen
-        this.bDpdY = false ; //dPad x ignore
-      }
-    }
-
-    public IEnumerator WhileRapidFire(){
-      while(this.brapd == true){
-        //          an.hState = cAnimn.eStateH.Plnt;
-        if(this.mt_TimeStep(this.sEM.fireRate)){
-          //	      Debug.Log ("Rapid Fire : " + Time.time); //HACK : time print doesn't match fireRate why???
-          this.doAttk(true);
-        }
-        else{
-          this.doAttk(false);
-        }
-        yield return null;
-      }
-      //		Debug.Log ("NO MORE RAPID FIRE"); //Only called once after while loop is complete
-      //        an.doAimg(0.0f)                 ; //reset gun to face forward
-      //        an.attkST = cAnimn.eStateB.Idle ; //release attack from powerup
-    }
-
-    private float stepDrtn = 0.0f;
-    private bool mt_TimeStep(float stepIncm){
-      if(Time.time > stepDrtn){
-        stepDrtn  = Time.time + stepIncm ;
-        return true                      ;    
-      }
-      else{
-        return false                     ;
-      }
-    }
-	
-#endregion
-
 	public void Init(){
-	  //set mod obj iLength based on emmitter list length
+	  //set mod obj iLength based on emitter list length
       this.sFP_mod.iLength = this.sEM.firePnts.Length ;
       this.sBL_mod.iLength = this.sEM.eBlt.Length     ;
 	}
@@ -179,10 +137,12 @@ namespace MTON.Class{
 	  Init();
     }
 
+#endregion
+
     // Update is called once per frame
     void Update () {
       if(Input.GetKeyDown(KeyCode.Space)){
-        this.doAttk(true);
+        this.doSinglFire(true);
 		this.doRapidFire(true);
       }
 	  else if(Input.GetKeyUp(KeyCode.Space)){
