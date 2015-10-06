dcb10cba0b79aa9848f53a9eb329ec17447ce784
Mike Ton <mike.ton@gmail.com>
2015-10-06 08:07:28 -0700

    Adding Menu System

---
+++ Unity/Assets/Shared/__MtonFrameWork/__Code/Classes/cEmit_Menu.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine        ;
+using System.Collections ;
+using UnityEngine.UI     ;
+using MTON.Interface     ;
+using MTON.Class         ;
+using MTON.Global        ;
+
+public class cEmit_Menu : MonoBehaviour, IEmit<Image>{ 
+
+#region iEmit implementation
+
+  public void Init(){  
+	if(__gCONSTANT._LEVEL.getSoundManager() != null){
+	  Debug.Log ("Sound Manager Exists");
+	}
+	else{
+	  Debug.Log ("Sound Manager Does Not Exists");
+	}
+  }
+  public void Play(){
+
+  }
+  public void Stop(){
+
+  }
+
+  public void OnComplete(){
+
+  }
+
+#endregion
+
+}
