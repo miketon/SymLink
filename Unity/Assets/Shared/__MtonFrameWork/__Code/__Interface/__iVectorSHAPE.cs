6657273c102ea84730a3aed5550b27becb1c7b70
Mike Ton <mike.ton@gmail.com>
2015-10-07 18:59:45 -0700

    Adding Vectrosity Object

--- Unity/Assets/Shared/__MtonFrameWork/__Code/__Interface/__iVectorSHAPE.cs
+++ Unity/Assets/Shared/__MtonFrameWork/__Code/__Interface/__iVectorSHAPE.cs
@@ -5,13 +5,8 @@ using DG.Tweening        ; //import DemiGiant DoTween
 namespace MTON.Interface{
 
   public interface iVectorSHAPE{ // Had to change from iTween to not conflict with iTween plug-in
-
-    bool bOnCompleteHide{ get; set; } // Hide on complete
-
-	//DOTween variables
-    Tween    tw_Cache { get; set; }
-    Sequence sq_Cache { get; set; }
-
+	// get pos based on % on curve
+	Vector3 vGetCurvePos(float IN_FLOAT);
   }
 
 }
