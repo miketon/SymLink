f5355e438f31a65af98b810ac4899907e9b85157
Mike Ton <mike.ton@gmail.com>
2015-10-20 21:20:10 -0700

    Adding oHint_ACTN spawn prefab on collision

--- Unity/Assets/Shared/__MtonFrameWork/__Code/Objects/HintBox/oHint_ACTN.cs
+++ Unity/Assets/Shared/__MtonFrameWork/__Code/Objects/HintBox/oHint_ACTN.cs
@@ -13,8 +13,17 @@ namespace MTON.codeObjects{
 	  Jump_Forward ,
 	  Dir_Change   ,
 	  Move_Forward ,
+	  SpawnPrefab  ,
     }
     public e_ActionType actionType;
+	public GameObject gSpawn;
+
+    public override void Start(){
+	  base.Start();
+	  if(gSpawn != null){
+		gSpawn = GameObject.Instantiate(gSpawn, this.transform.position, Quaternion.identity) as GameObject;
+	  }
+	}
 
 	public override void doHint (bool bHint, cInput cINPT){
 	  base.doHint (bHint, cINPT);
@@ -24,12 +33,22 @@ namespace MTON.codeObjects{
 	      cINPT.setActV(true)            ;
 	      this.tw.doCrouch(0.33f, 0.5f) ;
 		}
+		else if(actionType == e_ActionType.SpawnPrefab){
+		  if(this.gSpawn != null){
+		    this.gSpawn.SetActive(true);
+		  }
+		}
 	  }
 	  else if(bHint == false){
 		if(actionType == e_ActionType.Jump){
 	      cINPT.setJump(false)    ;
           cINPT.setActV(false)    ;
 	      this.tw.doCrouch(1.0f) ;
+		}
+		else if(actionType == e_ActionType.SpawnPrefab){
+		  if(this.gSpawn != null){
+		    this.gSpawn.SetActive(false);
+		  }
 		}
 	  }
 	}
